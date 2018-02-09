56be7456db030c96d04197bb5ef4faf72c925292
splinterific <markgpreston@gmail.com>
2018-02-09 15:15:22 +0000

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Courses_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
