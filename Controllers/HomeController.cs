616369c1c8feabe4197d7a547798585087dd138c
splinterific <markgpreston@gmail.com>
2018-02-12 22:42:56 +0000

    updated to add list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,6 +29,13 @@ namespace Courses_app.Controllers
             return View();
         }
 
+        public IActionResult List()
+        {
+            ViewData["Message"] = "This is the list page";
+
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
