a59d4adba809d1daba52599a3abf81a31534598a
splinterific <markgpreston@gmail.com>
2018-03-03 18:12:00 +0000

    updated Tips page and fixed FAQ

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -50,6 +50,11 @@ namespace Courses_app.Controllers
             return View();
         }
 
+        public IActionResult Tips()
+        {
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
