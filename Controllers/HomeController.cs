138d9b1e2d36af8028066f156c8b1e79a0895d2a
splinterific <markgpreston@gmail.com>
2018-03-03 09:33:15 +0000

    updated about page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace Courses_app.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult CourseList()
         {
             ViewData["Message"] = "Course Listings";
